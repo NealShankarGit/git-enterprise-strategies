a2386749cf19dddc50186bb1dfd174395a69df9c
Neal Shankar <nealshankar@Neals-MacBook-Pro.local>
2024-01-21 15:35:42 -0500

    Integrated changes from newText into App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! This is our team project.
-        </p>	
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p> Hello World! </p>
+        <p> My name is Neal Shankar and this is my project </p>
+        <p> Hello World! This is our team project. </p>
       </header>
     </div>
   );
